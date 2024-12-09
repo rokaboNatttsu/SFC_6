3e9142f7c1c6b965cbdbd9a6af0a676eb964878a
rokaboNatttsu <tenshikouhoshi@gmail.com>
2024-12-09 18:35:45 +0900

    ver. 0.2.0　株のキャピタルゲインへの税と資産税を追加

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-資産税（固定資産税や相続税）や、株式のキャピタルゲインへの税（ $\epsilon_xE+\epsilon_y\Delta p_e e_{-1}$ ）を追加したほうがリアルな挙動を示すかも。
+
+**汎用的なSFCモデルを目指す**
 
 - [TEM](#tem)
 - [BSM](#bsm)
@@ -11,68 +12,85 @@
 
 
 # TEM
-|                            |    労働者     |      投資家       |  企業(経常)   |   企業(資本)   |   民間金融機関    |     統合政府     | 合計 |
-| :------------------------- | :-----------: | :---------------: | :-----------: | :------------: | :---------------: | :--------------: | :--: |
-| 消費                       |    $-C_w$     |      $-C_i$       |     $+C$      |                |                   |                  | $0$  |
-| 投資                       |               |                   |     $+I$      |      $-I$      |                   |                  | $0$  |
-| 政府支出(公務員給与を除く) |               |                   |     $+G$      |                |                   |       $-G$       | $0$  |
-| 賃金                       |     $+W$      |                   |    $-W_f$     |                |      $-W_b$       |      $-W_g$      | $0$  |
-| 所得税                     |   $-T_{iw}$   |     $-T_{ii}$     |               |                |                   |      $+T_i$      | $0$  |
-| 付加価値税                 |               |                   |    $-T_v$     |                |                   |      $+T_v$      | $0$  |
-| 法人税                     |               |                   |   $-T_{ff}$   |                |     $-T_{fb}$     |      $+T_f$      | $0$  |
-| 企業利潤                   |               |      $+P_i$       |     $-P$      |     $+P_f$     |      $+P_b$       |                  | $0$  |
-| 金融機関から投資家への配当 |               |       $+S$        |               |                |       $-S$        |                  | $0$  |
-| 借入金利払い               | $-r_LL_{w-1}$ |                   | $-r_LL_{f-1}$ |                |   $+r_LL_{-1}$    |                  | $0$  |
-| 国債金利払い               |               | $+r_{GB}GB_{i-1}$ |               |                | $+r_{GB}GB_{b-1}$ | $-r_{GB}GB_{-1}$ | $0$  |
-| [メモ:純貸出]              |  [ $NL_w$ ]   |     [$NL_i$]      |               |    [$NL_f$]    |     [$NL_b$]      |     [$NL_g$]     | $0$  |
-| 預金の変化                 | $-\Delta M_w$ |   $-\Delta M_i$   |               | $-\Delta M_f$  |    $+\Delta M$    |                  | $0$  |
-| 借入金の変化               | $+\Delta L_w$ |                   |               | $+\Delta L_f$  |    $-\Delta L$    |                  | $0$  |
-| 株式純発行                 |               | $-p_e\Delta e_i$  |               | $+p_e\Delta e$ | $-p_e\Delta e_b$  |                  | $0$  |
-| 国債の変化                 |               |  $-\Delta GB_i$   |               |                |  $-\Delta GB_b$   |   $+\Delta GB$   | $0$  |
-| 現金の変化                 | $-\Delta H_w$ |                   |               |                |   $-\Delta H_b$   |   $+\Delta H$    | $0$  |
-| 合計                       |      $0$      |        $0$        |      $0$      |      $0$       |        $0$        |       $0$        |      |
+|                            | 労働者(経常)  | 労働者(資本)  |   投資家(経常)    |   投資家(資本)   |  企業(経常)   |   企業(資本)   |   民間金融機関    |     統合政府     | 合計 |
+| :------------------------- | :-----------: | :-----------: | :---------------: | :--------------: | :-----------: | :------------: | :---------------: | :--------------: | :--: |
+| 流動消費                   |    $-C_w$     |               |      $-C_i$       |                  |     $+C$      |                |                   |                  | $0$  |
+| 消費財資産購入             |               |   $-C_{aw}$   |                   |    $-C_{ai}$     |    $+C_a$     |                |                   |                  | $0$  |
+| 投資                       |               |               |                   |                  |     $+I$      |      $-I$      |                   |                  | $0$  |
+| 政府支出(公務員給与を除く) |               |               |                   |                  |     $+G$      |                |                   |       $-G$       | $0$  |
+| 賃金                       |     $+W$      |               |                   |                  |    $-W_f$     |                |      $-W_b$       |      $-W_g$      | $0$  |
+| 所得税                     |   $-T_{iw}$   |               |     $-T_{ii}$     |                  |               |                |                   |      $+T_i$      | $0$  |
+| 付加価値税                 |               |               |                   |                  |    $-T_v$     |                |                   |      $+T_v$      | $0$  |
+| 法人税                     |               |               |                   |                  |   $-T_{ff}$   |                |     $-T_{fb}$     |      $+T_f$      | $0$  |
+| 資産税                     |   $-T_{aw}$   |               |     $-T_{ai}$     |                  |   $-T_{af}$   |                |                   | $+T_a$ | $0$  |
+| 株式のキャピタルゲインへの税 | | | $-T_{ci}$ | | | | $-T_{cb}$ | $+T_c$ | $0$ |
+| 企業利潤                   |               |               |      $+P_i$       |                  |     $-P$      |     $+P_f$     |      $+P_b$       |                  | $0$  |
+| 金融機関から投資家への配当 |               |               |       $+IT$       |                  |               |                |       $-IT$       |                  | $0$  |
+| 借入金利払い               | $-r_LL_{w-1}$ |               |                   |                  | $-r_LL_{f-1}$ |                |   $+r_LL_{-1}$    |                  | $0$  |
+| 国債金利払い               |               |               | $+r_{GB}GB_{i-1}$ |                  |               |                | $+r_{GB}GB_{b-1}$ | $-r_{GB}GB_{-1}$ | $0$  |
+| 労働者貯蓄                 |    $-S_w$     |    $+S_w$     |                   |                  |               |                |                   |                  | $0$  |
+| 投資家貯蓄                 |               |               |      $-S_i$       |      $+S_i$      |               |                |                   |                  | $0$  |
+| [メモ:純貸出]              |               |  [ $NL_w$ ]   |                   |     [$NL_i$]     |               |    [$NL_f$]    |     [$NL_b$]      |     [$NL_g$]     | $0$  |
+| 預金の変化                 |               | $-\Delta M_w$ |                   |  $-\Delta M_i$   |               | $-\Delta M_f$  |    $+\Delta M$    |                  | $0$  |
+| 借入金の変化               |               | $+\Delta L_w$ |                   |                  |               | $+\Delta L_f$  |    $-\Delta L$    |                  | $0$  |
+| 株式純発行                 |               |               |                   | $-p_e\Delta e_i$ |               | $+p_e\Delta e$ | $-p_e\Delta e_b$  |                  | $0$  |
+| 国債の変化                 |               |               |                   |  $-\Delta GB_i$  |               |                |  $-\Delta GB_b$   |   $+\Delta GB$   | $0$  |
+| 現金の変化                 |               | $-\Delta H_w$ |                   |                  |               |                |   $-\Delta H_b$   |   $+\Delta H$    | $0$  |
+| 合計                       |      $0$      |      $0$      |        $0$        |       $0$        |      $0$      |      $0$       |        $0$        |       $0$        |      |
 
 
 # BSM
 |             | 労働者  | 資本家  |  企業   | 民間金融部門 | 統合政府 | 合計  |
 | :---------- | :-----: | :-----: | :-----: | :----------: | :------: | :---: |
-| 資本        |         |         |  $+K$   |              |          | $+K$  |
+| 資本        | $+K_w$  | $+K_i$  | $+K_f$  |              |          | $+K$  |
 | 預金        | $+M_w$  | $+M_i$  | $+M_f$  |     $-M$     |          |  $0$  |
 | 借入金      | $-L_w$  |         | $-L_f$  |     $+L$     |          |  $0$  |
 | 株式/資本金 |         | $+E_i$  |  $-E$   |    $+E_b$    |          |  $0$  |
 | 国債        |         | $+GB_i$ |         |   $+GB_b$    |  $-GB$   |  $0$  |
 | 現金        | $+H_w$  |         |         |    $+H_b$    |   $-H$   |  $0$  |
 | 純資産      | $-NW_w$ | $-NW_i$ | $-NW_f$ |   $-NW_b$    | $-NW_g$  | $-NW$ |
-| 合計        |   $0$   |   $0$   |   $0$   |     $0$      |   $0$    | $0$ |
+| 合計        |   $0$   |   $0$   |   $0$   |     $0$      |   $0$    |  $0$  |
 
 # 式一覧
 上から順に計算する
-$W_f=(1-\lambda_w)W_{f-1}+\lambda_w\delta(C_{-1}+I_{-1}+G_{-1})$
-$W_b=(1-\lambda_w)W_{b-1}+\lambda_w\delta(P_{b-1}+r_LL_{-1}+r_{GB}GB_{b-1})$
+$W_f=(1-\lambda_w)W_{f-1}+\lambda_w\delta(C_{-1}+C_{a-1}+I_{-1}+G_{-1})$
+$W_b=(1-\lambda_w)W_{b-1}+\lambda_w\delta(-T_{cb-1}+P_{b-1}-IT_{-1}+r_LL_{-1}+r_{GB}GB_{b-1})$
 $W_g=W_{g0}\exp(\gamma t)$
 $W=W_f+W_b+W_g$
 $T_{iw}=\epsilon_1 W$
+$T_{ci}=\max\{0, \epsilon_7(E_{i-1}-\Delta E_{i-1})+\epsilon_8\Delta p_{e-1}(e_{i-1}-\Delta e_{i-1})\}$
+$T_{cb}=\max\{0, \epsilon_7(E_{b-1}-\Delta E_{b-1})+\epsilon_8\Delta p_{e-1}(e_{b-1}-\Delta e_{b-1})\}$
+$T_c=T_{ci}+T_{cb}$
 $G=G_0\exp(\gamma t)$
-$I=\max[0, \min\{(\frac{C+G}{\beta_1 K_{-1}}-u^T)K_{-1}+\beta_2K_{-1}, \beta_3(M_{f-1}-L_{f-1})\}]$
-$C_w=\alpha_1(W-T_{iw}-r_LL_{w-1})+\alpha_2NW_{w-1}$
-$C_i=\alpha_3(P_{i-1}-T_{ii-1}+r_{GB}GB_{i-1})$
+$C_w=\alpha_4\{\alpha_1(W-T_{iw}-T_{aw}-r_LL_{w-1})+\alpha_2(NW_{w-1}-K_{w-1})\}$
+$C_i=\alpha_5\alpha_3(P_{i-1}-T_{ii}-T_{ai}+r_{GB}GB_{i-1})$
 $C=C_w+C_i$
-$T_v=\epsilon_2(C+G+I)$
-$T_{ff}=\max\{0,\epsilon_3(C+G+I-W_f-T_v-r_LL_{f-1})\}$
-$P=C+G+I-W_f-T_v-T_{ff}-r_LL_{f-1}$
+$C_{aw}=(1-\alpha_4)\{\alpha_1(W-T_{iw}-T_{aw}-r_LL_{w-1})+\alpha_2(NW_{w-1}-K_{w-1})\}$
+$C_{ai}=(1-\alpha_5)\alpha_3(P_{i-1}-T_{ii}-T_{ai}+r_{GB}GB_{i-1})$
+$C_a=C_{aw}+C_{ai}$
+$I=\max[0, \min\{(\frac{C+C_a+G}{\beta_1 K_{f-1}}-u^T)K_{f-1}+\beta_2K_{f-1}, \beta_3(M_{f-1}-L_{f-1})\}]$
+$T_v=\epsilon_2(C+C_a+G+I)$
+$T_{ff}=\max\{0,\epsilon_3(C+C_a+G+I-W_f-T_v-T_{af}-r_LL_{f-1})\}$
+$T_{aw}=\epsilon_5K_{w-1}$
+$T_{ai}=\epsilon_5K_{i-1}$
+$T_{af}=\epsilon_6K_{f-1}$
+$T_a=T_{aw}+T_{ai}+T_{af}$
+$P=C+C_a+G+I-W_f-T_v-T_{ff}-T_{af}-r_LL_{f-1}$
 $P_i=\zeta(P-I)\frac{E_{i-1}}{E_{-1}}$
 $P_b=\zeta(P-I)\frac{E_{b-1}}{E_{-1}}$
 $P_f=P-P_i-P_b$
-$S=\theta P_b$
-$T_{ii}=\epsilon_4(P_i+S+r_{GB}GB_{i-1})$
+$IT=\theta P_b$
+$T_{ii}=\epsilon_4(P_i+IT+r_{GB}GB_{i-1})$
 $T_i=T_{iw}+T_{ii}$
 $T_{fb}=\max\{0, \epsilon_3(-W_b+P_b+r_LL_{-1}+r_{GB}GB_{b-1})\}$
 $T_f=T_{ff}+T_{fb}$
-$NL_w=-C_w+W-T_{iw}-r_LL_{w-1}$
-$NL_i=-C_i+P_i+S-T_{ii}+r_{GB}GB_{i-1}$
+$S_w=-C_w+W-T_{iw}-T_{aw}-r_LL_{w-1}$
+$S_i=-C_i-T_{ii}-T_{ai}-T_{ci}+P_i+IT+r_{GB}GB_{i-1}$
+$NL_w=-C_{aw}+S_w$
+$NL_i=-C_{ai}+S_i$
 $NL_f=P_f-I$
-$NL_b=-W_b-T_{fb}+P_b-S+r_LL_{-1}+r_{GB}GB_{b-1}$
-$NL_g=-G-W_g+T_i+T_v+T_f-r_{GB}GB_{-1}$
+$NL_b=-W_b-T_{fb}-T_{cb}+P_b-S+r_LL_{-1}+r_{GB}GB_{b-1}$
+$NL_g=-G-W_g+T_i+T_v+T_f+T_a+T_c-r_{GB}GB_{-1}$
 $L_w=\eta_1(W-T_i)$
 $\Delta L_w=L_w-L_{w-1}$
 $H_w=\eta_2C_w$
@@ -83,7 +101,7 @@ $V_i=M_i+E_i+GB_i$
 $V_b=E_b+GB_b$
 $V_i^e=(1-\lambda_V)V_{i-1}^e+\lambda_VV_{i-1}\frac{V_{i-1}}{V_{i-1}-\Delta V_{i-1}}$
 $V_b^e=(1-\lambda_V)V_{b-1}^e+\lambda_VV_{b-1}\frac{V_{b-1}}{V_{b-1}-\Delta V_{b-1}}$
-$Δe=\frac{\eta_3(I-\beta_2K_{-1})}{p_{e-1}}$
+$Δe=\frac{\eta_3(I-\beta_2K_{f-1})}{p_{e-1}}$
 $e=e_{-1}+\Delta e$
 $L_f=\max(0, (1+\eta_4)W-M_{f-1})$
 $\Delta L_f=L_f-L_{f-1}$
@@ -151,10 +169,13 @@ $E_i=E_{i-1}+\Delta E_i$
 $E_b=E_{b-1}+\Delta E_b$
 $\Delta E=\Delta E_i+\Delta E_b$
 $E=E_{-1}+\Delta E$
-$K=(1-\beta_2)K_{-1}+I$
-$NW_w=M_w-L_w+H_w$
-$NW_i=M_i+E_i+GB_i$
-$NW_f=M_f-L_f-E+K$
+$K_f=(1-\beta_2)K_{f-1}+I$
+$K_w=(1-\beta_4)K_{w-1}+C_{aw}$
+$K_i=(1-\beta_4)K_{i-1}+C_{ai}$
+$K=K_w+K_i+K_f$
+$NW_w=K_w+M_w-L_w+H_w$
+$NW_i=K_i+M_i+E_i+GB_i$
+$NW_f=M_f-L_f-E+K_f$
 $NW_b=-M+L+E_b+GB_b+H_b$
 $NW_g=-GB-H$
 $NW=K$
@@ -170,11 +191,11 @@ $H=H_w+H_b$
 $NW=NW_w+NW_i+NW_f+NW_b+NW_g$
 
 # 外生変数とパラメータ
-$(\alpha_1, \alpha_2, \alpha_3)=(0.9, 0.05, 0.2)$
-$(\beta_1, \beta_2, \beta_3)=(1,0.05,1)$
+$(\alpha_1, \alpha_2, \alpha_3, \alpha_4, \alpha_5)=(0.9, 0.05, 0.2, 0.9, 0.7)$
+$(\beta_1, \beta_2, \beta_3, \beta_4)=(1,0.05,1, 0.05)$
 $\gamma=0.02$
 $\delta=0.65$
-$(\epsilon_1, \epsilon_2, \epsilon_3, \epsilon_4)=(0.3,0.1,0.2,0.2)$
+$(\epsilon_1, \epsilon_2, \epsilon_3, \epsilon_4, \epsilon_5, \epsilon_6, \epsilon_7, \epsilon_8)=(0.3,0.1,0.2,0.2, 0.05, 0.02, 0.1, 0.1)$
 $\zeta=0.3$
 $(\eta_1,\eta_2,\eta_3,\eta_4,\eta_5)=(5,0.1,0.3,0.5,0.01)$
 $\theta=0.3$
@@ -184,7 +205,7 @@ $\begin{pmatrix}  \lambda_{11} & \lambda_{12} & \lambda_{13}\\  \lambda_{21} & \
 $(\lambda_{40}, \lambda_{50})=(0.5, 0.5)$
 $\begin{pmatrix}  \lambda_{41} & \lambda_{42}\\  \lambda_{51} & \lambda_{52}\end{pmatrix}=\begin{pmatrix}  0.7 & -0.7\\  -0.7 & 0.7\end{pmatrix}$
 $(r_L,r_{GB})=(0.01, 0.03)$
-$u^T=0.7$
+$u^T=0.8$
 
 # 再現されるか確かめたい現象
 ## GDP停滞中に債務残高対GDP比の推移
--- main.jl
+++ main.jl
@@ -2,10 +2,10 @@ using StatsPlots
 
 N=201
 
-Cw, Ci, C, I, G = zeros(N), zeros(N), zeros(N), zeros(N), zeros(N)
+Cw, Ci, C, Caw, Cai, Ca, I, G = zeros(N), zeros(N), zeros(N), zeros(N), zeros(N), zeros(N), zeros(N), zeros(N)
 W, Wf, Wb, Wg = zeros(N), zeros(N), zeros(N), zeros(N)
-Tiw, Tii, Ti, Tv, Tff, Tfb, Tf = zeros(N), zeros(N), zeros(N), zeros(N), zeros(N), zeros(N), zeros(N)
-Pi, P, Pf, Pb, S = zeros(N), zeros(N), zeros(N), zeros(N), zeros(N)
+Tiw, Tii, Ti, Tv, Tff, Tfb, Tf, Taw, Tai, Taf, Ta, Tci, Tcb, Tc = zeros(N), zeros(N), zeros(N), zeros(N), zeros(N), zeros(N), zeros(N), zeros(N), zeros(N), zeros(N), zeros(N), zeros(N), zeros(N), zeros(N)
+Pi, P, Pf, Pb, IT, Sw, Si = zeros(N), zeros(N), zeros(N), zeros(N), zeros(N), zeros(N), zeros(N)
 NLw, NLi, NLf, NLb, NLg = zeros(N), zeros(N), zeros(N), zeros(N), zeros(N)
 ΔMw, ΔMi, ΔMf, ΔM = zeros(N), zeros(N), zeros(N), zeros(N)
 ΔLw, ΔLf, ΔL = zeros(N), zeros(N), zeros(N)
@@ -13,7 +13,7 @@ NLw, NLi, NLf, NLb, NLg = zeros(N), zeros(N), zeros(N), zeros(N), zeros(N)
 ΔEi, ΔEb, ΔE = zeros(N), zeros(N), zeros(N)
 ΔGBi, ΔGBb, ΔGB = zeros(N), zeros(N), zeros(N)
 ΔHw, ΔHb, ΔH = zeros(N), zeros(N), zeros(N)
-K = zeros(N)
+Kf, Kw, Ki, K = zeros(N), zeros(N), zeros(N), zeros(N)
 Mw, Mi, Mf, M = zeros(N), zeros(N), zeros(N), zeros(N)
 Lw, Lf, L = zeros(N), zeros(N), zeros(N)
 ei, eb, e = zeros(N), zeros(N), zeros(N)
@@ -24,14 +24,14 @@ NWw, NWi, NWf, NWb, NWg, NW = zeros(N), zeros(N), zeros(N), zeros(N), zeros(N), 
 Vi, Vb, Vie, Vbe, ΔVi, ΔVb = zeros(N), zeros(N), zeros(N), zeros(N), zeros(N), zeros(N)
 Eie, GBie, Ebe, GBbe = zeros(N), zeros(N), zeros(N), zeros(N)
 pe, Δpe = zeros(N), zeros(N)
-rL, rGB, uT = 0.01, 0.03, 0.7
-α1, α2, α3 = 0.95, 0.05, 0.02
-β1, β2, β3 = 1.5, 0.05, 1
+rL, rGB, uT = 0.01, 0.03, 0.8
+α1, α2, α3, α4, α5 = 0.95, 0.05, 0.02, 0.9, 0.7
+β1, β2, β3, β4 = 1, 0.05, 1, 0.05
 γ = 0.02
 δ = 0.7
-ϵ1, ϵ2, ϵ3, ϵ4 = 0.3, 0.1, 0.2, 0.2
+ϵ1, ϵ2, ϵ3, ϵ4, ϵ5, ϵ6, ϵ7, ϵ8 = 0.3, 0.1, 0.2, 0.2, 0.05, 0.02, 0.02, 0.1
 ζ = 0.3
-η1, η2, η3, η4, η5 = 5, 0.5, 0.3, 1, 0.01
+η1, η2, η3, η4, η5 = 5, 0.5, 0.5, 1, 0.01
 θ = 0.3
 λV, λw = 0.5, 0.5
 λ10, λ20, λ30 = 0.2, 0.4, 0.4
@@ -44,7 +44,7 @@ rL, rGB, uT = 0.01, 0.03, 0.7
 G0, Wg0 = 1,1
 G[1], Wg[1], W[1] = G0, Wg0, Wg0
 pe[1] = 1
-K[1] = 6
+Kf[1], K[1] = 6, 6
 Mw[1], Mi[1], Mf[1], M[1] = 1,1,1,3
 ei[1], eb[1], e[1] = 2,2,4
 Ei[1], Eb[1], E[1] = pe[1]*ei[1], pe[1]*eb[1], pe[1]*e[1]
@@ -52,7 +52,7 @@ GBi[1], GBb[1], GB[1] = 1,1,2
 Hb[1], H[1] = η5*M[1],η5*M[1]
 NWw[1] = Mw[1] - Lw[1] + Hw[1]
 NWi[1] = Mi[1] + Ei[1] + GBi[1]
-NWf[1] = Mf[1] - Lf[1] - E[1] + K[1]
+NWf[1] = Mf[1] - Lf[1] - E[1] + Kf[1]
 NWb[1] = -M[1] + L[1] + Eb[1] + GBb[1] + Hb[1]
 NWg[1] = -GB[1] - H[1]
 NW[1] = K[1]
@@ -60,32 +60,44 @@ Vi[1], Vb[1], Vie[1], Vbe[1] = Mi[1]+Ei[1]+GBi[1], Eb[1]+GBb[1], Mi[1]+Ei[1]+GBi
 
 function run(time_range::UnitRange)
     for t=time_range
-        Wf[t] = (1-λw)*Wf[t-1] + λw*δ*(C[t-1]+I[t-1]+G[t-1])
-        Wb[t] = (1-λw)*Wb[t-1] + λw*δ*(Pb[t-1]+rL*L[t-1]+rGB*GBb[t-1])
+        Wf[t] = (1-λw)*Wf[t-1] + λw*δ*(C[t-1]+Ca[t-1]+I[t-1]+G[t-1])
+        Wb[t] = (1-λw)*Wb[t-1] + λw*δ*(-Tcb[t-1]-IT[t-1]+Pb[t-1]+rL*L[t-1]+rGB*GBb[t-1])
         Wg[t] = Wg0*exp(γ*t)
         W[t] = Wf[t] + Wb[t] + Wg[t]
         Tiw[t] = ϵ1*W[t]
+        Tci[t] = max(0, ϵ7*(Ei[t-1]-ΔEi[t-1]) + ϵ8*Δpe[t-1]*(ei[t-1]-Δei[t-1]))
+        Tcb[t] = max(0, ϵ7*(Eb[t-1]-ΔEb[t-1]) + ϵ8*Δpe[t-1]*(eb[t-1]-Δeb[t-1]))
+        Tc[t]  = Tci[t] + Tcb[t]
         G[t] = G0*exp(γ*t)
-        I[t] = max(0, min(((C[t]+G[t])/(β1*K[t-1]) - uT)*K[t-1] + β2*K[t-1], β3*(Mf[t-1]-Lf[t-1])))
-        Cw[t] = α1*(W[t]-Tiw[t]-rL*Lw[t-1]) + α2*NWw[t-1]
-        Ci[t] = α3*(Pi[t-1]-Tii[t-1]+rGB*GBi[t-1])
+        Cw[t] = α4*(α1*(W[t]-Tiw[t]-Taw[t]-rL*Lw[t-1]) + α2*(NWw[t-1]-Kw[t-1]))
+        Ci[t] = α5*α3*(Pi[t-1]-Tii[t-1]-Tai[t]+rGB*GBi[t-1])
         C[t] = Cw[t] + Ci[t]
-        Tv[t] = ϵ2*(C[t]+G[t]+I[t])
-        Tff[t] = max(0, ϵ3*(C[t]+G[t]+I[t]-Wf[t]-Tv[t]-rL*Lf[t-1]))
-        P[t] = C[t]+G[t]+I[t]-Wf[t]-Tv[t]-Tff[t]-rL*Lf[t-1]
+        Caw[t] = (1 - α4)*(α1*(W[t]-Tiw[t]-Taw[t]-rL*Lw[t-1]) + α2*(NWw[t-1]-Kw[t-1]))
+        Cai[t] = (1 - α5)*α3*(Pi[t-1]-Tii[t-1]-Tai[t]+rGB*GBi[t-1])
+        Ca[t] = Caw[t] + Cai[t]
+        I[t] = max(0, min(((C[t]+Ca[t]+G[t])/(β1*Kf[t-1]) - uT)*Kf[t-1] + β2*Kf[t-1], β3*(Mf[t-1]-Lf[t-1])))
+        Tv[t] = ϵ2*(C[t]+Ca[t]+G[t]+I[t])
+        Tff[t] = max(0, ϵ3*(C[t]+Ca[t]+G[t]+I[t]-Wf[t]-Tv[t]-Taf[t]-rL*Lf[t-1]))
+        Taw[t] = ϵ5*Kw[t-1]
+        Tai[t] = ϵ5*Ki[t-1]
+        Taf[t] = ϵ6*Kf[t-1]
+        Ta[t] = Taw[t] + Tai[t] + Taf[t]
+        P[t] = C[t]+Ca[t]+G[t]+I[t]-Wf[t]-Tv[t]-Tff[t]-Taf[t]-rL*Lf[t-1]
         Pi[t] = ζ*(P[t]-I[t])*Ei[t-1]/E[t-1]
         Pb[t] = ζ*(P[t]-I[t])*Eb[t-1]/E[t-1]
         Pf[t] = P[t]-Pi[t]-Pb[t]
-        S[t] = θ*Pb[t]
-        Tii[t] = ϵ4*(Pi[t]+S[t]+rGB*GBi[t-1])
+        IT[t] = θ*Pb[t]
+        Tii[t] = ϵ4*(Pi[t]+IT[t]+rGB*GBi[t-1])
         Ti[t] = Tiw[t] + Tii[t]
-        Tfb[t] = max(0, ϵ3*(-Wb[t]+Pb[t]+rL*L[t-1]+rGB*GBb[t-1]))
+        Tfb[t] = max(0, ϵ3*(-Wb[t]-Tcb[t]+Pb[t]+rL*L[t-1]+rGB*GBb[t-1]))
         Tf[t] = Tff[t] + Tfb[t]
-        NLw[t] = -Cw[t]+W[t]-Tiw[t]-rL*Lw[t-1]
-        NLi[t] = -Ci[t]+Pi[t]+S[t]-Tii[t]+rGB*GBi[t-1]
+        Sw[t] = -Cw[t]+W[t]-Tiw[t]-Taw[t]-rL*Lw[t-1]
+        Si[t] = -Ci[t]-Tii[t]-Tai[t]-Tci[t]+Pi[t]+IT[t]+rGB*GBi[t-1]
+        NLw[t] = -Caw[t]+Sw[t]
+        NLi[t] = -Cai[t]+Si[t]
         NLf[t] = Pf[t] - I[t]
-        NLb[t] = -Wb[t] - Tfb[t] + Pb[t] - S[t] + rL*L[t-1] + rGB*GBb[t-1]
-        NLg[t] = -G[t] - Wg[t] + Ti[t] + Tv[t] + Tf[t] - rGB*GB[t-1]
+        NLb[t] = -Wb[t] - Tfb[t] - Tcb[t] + Pb[t] - IT[t] + rL*L[t-1] + rGB*GBb[t-1]
+        NLg[t] = -G[t] - Wg[t] + Ti[t] + Tv[t] + Tf[t] + Ta[t] + Tc[t] - rGB*GB[t-1]
         Lw[t] = η1*(W[t] - Ti[t])
         ΔLw[t] = Lw[t] - Lw[t-1]
         Hw[t] = η2*Cw[t]
@@ -94,7 +106,7 @@ function run(time_range::UnitRange)
         Mw[t] = Mw[t-1] + ΔMw[t]
         Vie[t] = (1-λV)*Vie[t-1] + λV*Vi[t-1]^2/(Vi[t-1]-ΔVi[t-1])
         Vbe[t] = (1-λV)*Vbe[t-1] + λV*Vb[t-1]^2/(Vb[t-1]-ΔVb[t-1])
-        Δe[t] = η3*(I[t]-β2*K[t-1])/pe[t-1]
+        Δe[t] = η3*(I[t]-β2*Kf[t-1])/pe[t-1]
         e[t] = e[t-1] + Δe[t]
         Lf[t] = max(0, (1+η4)*W[t]-Mf[t-1])
         ΔLf[t] = Lf[t] - Lf[t-1]
@@ -134,127 +146,156 @@ function run(time_range::UnitRange)
         Vb[t] = Eb[t] + GBb[t]
         ΔE[t] = ΔEi[t] + ΔEb[t]
         E[t] = E[t-1] + ΔE[t]
-        K[t] = (1-β2)*K[t-1] + I[t]
-        NWw[t] = Mw[t] - Lw[t] +Hw[t]
-        NWi[t] = Mi[t] + Ei[t] + GBi[t]
-        NWf[t] = Mf[t] - Lf[t] - E[t] + K[t]
-        NWb[t] = -M[t] +L[t] + Eb[t] + GBb[t] + Hb[t]
+        Kf[t] = (1-β2)*Kf[t-1] + I[t]
+        Kw[t] = (1-β4)*Kw[t-1] + Caw[t]
+        Ki[t] = (1-β4)*Ki[t-1] + Cai[t]
+        K[t] = Kf[t] + Kw[t] + Ki[t]
+        NWw[t] = Kw[t] + Mw[t] - Lw[t] +Hw[t]
+        NWi[t] = Ki[t] + Mi[t] + Ei[t] + GBi[t]
+        NWf[t] = Mf[t] - Lf[t] - E[t] + Kf[t]
+        NWb[t] = -M[t] + L[t] + Eb[t] + GBb[t] + Hb[t]
         NWg[t] = -GB[t] - H[t]
         NW[t] = K[t]
     end
 end
 
-run(2:N)
+run(2:N-40)
 
+run(N-39:N)
 
-plot(C[end-50:end], label="C")
-plot!(Cw[end-50:end], label="Cw")
-plot!(Ci[end-50:end], label="Ci")
-plot!(G[end-50:end], label="G")
-plot!(I[end-50:end], label="I")
-savefig("figs/Y.png")
+function all_plot()
+    plot(C[end-50:end], label="C")
+    plot!(Ca[end-50:end], label="C")
+    plot!(G[end-50:end], label="G")
+    plot!(I[end-50:end], label="I")
+    savefig("figs/Y.png")
 
-plot(Ti[end-50:end], label="Ti")
-plot!(Tiw[end-50:end], label="Tiw")
-plot!(Tii[end-50:end], label="Tii")
-savefig("figs/Ti.png")
+    plot(C[end-50:end], label="C")
+    plot!(Cw[end-50:end], label="Cw")
+    plot!(Ci[end-50:end], label="Ci")
+    plot!(Ca[end-50:end], label="C")
+    plot!(Caw[end-50:end], label="Caw")
+    plot!(Cai[end-50:end], label="Cai")
+    savefig("figs/C.png")
 
-plot(Tf[end-50:end], label="Tf")
-plot!(Tfb[end-50:end], label="Tfb")
-plot!(Tff[end-50:end], label="Tff")
-savefig("figs/Tf.png")
+    plot(Ti[end-50:end], label="Ti")
+    plot!(Tiw[end-50:end], label="Tiw")
+    plot!(Tii[end-50:end], label="Tii")
+    savefig("figs/Ti.png")
 
-plot(Ti[end-50:end], label="Ti")
-plot!(Tv[end-50:end], label="Tv")
-plot!(Tf[end-50:end], label="Tf")
-savefig("figs/T.png")
+    plot(Ta[end-50:end], label="Ta")
+    plot!(Taw[end-50:end], label="Taw")
+    plot!(Tai[end-50:end], label="Tai")
+    plot!(Taf[end-50:end], label="Taf")
+    savefig("figs/Ta.png")
 
-plot(W[end-50:end], label="W")
-plot!(Wf[end-50:end], label="Wf")
-plot!(Wb[end-50:end], label="Wb")
-plot!(Wg[end-50:end], label="Wg")
-savefig("figs/W.png")
+    plot(Tf[end-50:end], label="Tf")
+    plot!(Tfb[end-50:end], label="Tfb")
+    plot!(Tff[end-50:end], label="Tff")
+    savefig("figs/Tf.png")
 
-plot(S[end-50:end], label="S")
-plot!(P[end-50:end], label="P")
-plot!(Pb[end-50:end], label="Pb")
-plot!(Pi[end-50:end], label="Pi")
-plot!(Pf[end-50:end], label="Pf")
-savefig("figs/P_and_S.png")
+    plot(Tc[end-50:end], label="Tc")
+    plot!(Tcb[end-50:end], label="Tcb")
+    plot!(Tci[end-50:end], label="Tci")
+    savefig("figs/Tc.png")
 
-plot(Mw[end-50:end], label="Mw")
-plot!(Mi[end-50:end], label="Mi")
-plot!(Mf[end-50:end], label="Mf")
-plot!(M[end-50:end], label="M")
-savefig("figs/M.png")
+    plot(Ti[end-50:end], label="Ti")
+    plot!(Tv[end-50:end], label="Tv")
+    plot!(Tf[end-50:end], label="Tf")
+    plot!(Ta[end-50:end], label="Ta")
+    plot!(Tc[end-50:end], label="Tc")
+    savefig("figs/T.png")
 
-plot(ΔMw[end-50:end], label="ΔMw")
-plot!(ΔMi[end-50:end], label="ΔMi")
-plot!(ΔMf[end-50:end], label="ΔMf")
-plot!(ΔM[end-50:end], label="ΔM")
-savefig("figs/ΔM.png")
+    plot(W[end-50:end], label="W")
+    plot!(Wf[end-50:end], label="Wf")
+    plot!(Wb[end-50:end], label="Wb")
+    plot!(Wg[end-50:end], label="Wg")
+    savefig("figs/W.png")
 
-plot(e[end-50:end], label="e")
-plot!(ei[end-50:end], label="ei")
-plot!(eb[end-50:end], label="eb")
-savefig("figs/e.png")
+    plot(IT[end-50:end], label="IT")
+    plot!(P[end-50:end], label="P")
+    plot!(Pb[end-50:end], label="Pb")
+    plot!(Pi[end-50:end], label="Pi")
+    plot!(Pf[end-50:end], label="Pf")
+    savefig("figs/P_and_IT.png")
 
-plot(Δe[end-50:end], label="Δe")
-plot!(Δei[end-50:end], label="Δei")
-plot!(Δeb[end-50:end], label="Δeb")
-savefig("figs/Δe.png")
+    plot(Mw[end-50:end], label="Mw")
+    plot!(Mi[end-50:end], label="Mi")
+    plot!(Mf[end-50:end], label="Mf")
+    plot!(M[end-50:end], label="M")
+    savefig("figs/M.png")
 
-plot(pe[end-50:end], label="pe")
-savefig("figs/pe.png")
+    plot(ΔMw[end-50:end], label="ΔMw")
+    plot!(ΔMi[end-50:end], label="ΔMi")
+    plot!(ΔMf[end-50:end], label="ΔMf")
+    plot!(ΔM[end-50:end], label="ΔM")
+    savefig("figs/ΔM.png")
 
-plot(K[end-50:end], label="K")
-plot!(I[end-50:end], label="I")
-savefig("figs/K_and_I.png")
+    plot(e[end-50:end], label="e")
+    plot!(ei[end-50:end], label="ei")
+    plot!(eb[end-50:end], label="eb")
+    savefig("figs/e.png")
 
-plot(L[end-50:end], label="L")
-plot!(Lw[end-50:end], label="Lw")
-plot!(Lf[end-50:end], label="Lf")
-savefig("figs/L.png")
+    plot(Δe[end-50:end], label="Δe")
+    plot!(Δei[end-50:end], label="Δei")
+    plot!(Δeb[end-50:end], label="Δeb")
+    savefig("figs/Δe.png")
 
-plot(ΔL[end-50:end], label="ΔL")
-plot!(ΔLw[end-50:end], label="ΔLw")
-plot!(ΔLf[end-50:end], label="ΔLf")
-savefig("figs/ΔL.png")
+    plot(pe[end-50:end], label="pe")
+    savefig("figs/pe.png")
 
-plot(H[end-50:end], label="H")
-plot!(Hw[end-50:end], label="Hw")
-plot!(Hb[end-50:end], label="Hb")
-savefig("figs/H.png")
+    plot(K[end-50:end], label="K")
+    plot!(Kf[end-50:end], label="Kf")
+    plot!(Ki[end-50:end], label="Ki")
+    plot!(Kw[end-50:end], label="Kw")
+    savefig("figs/K.png")
 
-plot(ΔH[end-50:end], label="ΔH")
-plot!(ΔHw[end-50:end], label="ΔHw")
-plot!(ΔHb[end-50:end], label="ΔHb")
-savefig("figs/ΔH.png")
+    plot(L[end-50:end], label="L")
+    plot!(Lw[end-50:end], label="Lw")
+    plot!(Lf[end-50:end], label="Lf")
+    savefig("figs/L.png")
 
-plot(GB[end-50:end], label="GB")
-plot!(GBi[end-50:end], label="GBi")
-plot!(GBb[end-50:end], label="GBb")
-savefig("figs/GB.png")
+    plot(ΔL[end-50:end], label="ΔL")
+    plot!(ΔLw[end-50:end], label="ΔLw")
+    plot!(ΔLf[end-50:end], label="ΔLf")
+    savefig("figs/ΔL.png")
 
-plot(ΔGB[end-50:end], label="ΔGB")
-plot!(ΔGBi[end-50:end], label="ΔGBi")
-plot!(ΔGBb[end-50:end], label="ΔGBb")
-savefig("figs/ΔGB.png")
+    plot(H[end-50:end], label="H")
+    plot!(Hw[end-50:end], label="Hw")
+    plot!(Hb[end-50:end], label="Hb")
+    savefig("figs/H.png")
 
-plot(NWw[end-50:end], label="NWw")
-plot!(NWi[end-50:end], label="NWi")
-plot!(NWf[end-50:end], label="NWf")
-plot!(NWb[end-50:end], label="NWb")
-plot!(NWg[end-50:end], label="NWg")
-savefig("figs/NW.png")
+    plot(ΔH[end-50:end], label="ΔH")
+    plot!(ΔHw[end-50:end], label="ΔHw")
+    plot!(ΔHb[end-50:end], label="ΔHb")
+    savefig("figs/ΔH.png")
 
-plot(NLw[end-50:end], label="NLw")
-plot!(NLi[end-50:end], label="NLi")
-plot!(NLf[end-50:end], label="NLf")
-plot!(NLb[end-50:end], label="NLb")
-plot!(NLg[end-50:end], label="NLg")
-savefig("figs/NL.png")
+    plot(GB[end-50:end], label="GB")
+    plot!(GBi[end-50:end], label="GBi")
+    plot!(GBb[end-50:end], label="GBb")
+    savefig("figs/GB.png")
 
-plot(((H+GB)./(C+G+I))[end-50:end], label="(H+GB)/Y")
-plot!(zeros(51), label=nothing)
-savefig("figs/Debt-to-GDP ratio.png")
+    plot(ΔGB[end-50:end], label="ΔGB")
+    plot!(ΔGBi[end-50:end], label="ΔGBi")
+    plot!(ΔGBb[end-50:end], label="ΔGBb")
+    savefig("figs/ΔGB.png")
+
+    plot(NWw[end-50:end], label="NWw")
+    plot!(NWi[end-50:end], label="NWi")
+    plot!(NWf[end-50:end], label="NWf")
+    plot!(NWb[end-50:end], label="NWb")
+    plot!(NWg[end-50:end], label="NWg")
+    savefig("figs/NW.png")
+
+    plot(NLw[end-50:end], label="NLw")
+    plot!(NLi[end-50:end], label="NLi")
+    plot!(NLf[end-50:end], label="NLf")
+    plot!(NLb[end-50:end], label="NLb")
+    plot!(NLg[end-50:end], label="NLg")
+    savefig("figs/NL.png")
+
+    plot(((H+GB)./(C+G+I))[end-50:end], label="(H+GB)/Y")
+    plot!(zeros(51), label=nothing, color="Gray")
+    savefig("figs/Debt-to-GDP ratio.png")
+end
+all_plot()
